74333568b309a6b4c7a29e55c6c3e0f821a8a36a
FelipeAlves14 <favasconcelos09@gmail.com>
2025-10-26 19:07:36 -0300

    :tada: feat: inicia projeto de landing page

---
+++ fisioPage/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import marcaGrafica from "../assets/marca.png";
+
+export default function Header() {
+  return (
+    <div>
+      <img src={marcaGrafica} alt="Walterclay Abreu" />
+    </div>
+  );
+}
