803d0515f5bb3d00eda2ca6b545578d9f5f1d07a
FelipeAlves14 <favasconcelos09@gmail.com>
2025-10-27 16:10:28 -0300

    :sparkles: finaliza cabeçalho da página

--- fisioPage/src/components/Header.tsx
+++ fisioPage/src/components/Header.tsx
@@ -1,9 +1,19 @@
-import marcaGrafica from "../assets/marca.png";
+import Button from "./Button";
 
 export default function Header() {
   return (
-    <div>
-      <img src={marcaGrafica} alt="Walterclay Abreu" />
-    </div>
+    <header className="flex items-center justify-between sticky">
+      <div className="bg-[url(/fundo-marca.png)] bg-no-repeat p-4 w-full">
+        <img src="/marca.png" alt="Walterclay Abreu" />
+      </div>
+      <div className="flex justify-evenly text-(--option-color) w-full">
+        <a href="formacoes">Formações</a>
+        <a href="servicos">Serviços</a>
+        <a href="localizacoes">Localizações</a>
+      </div>
+      <div className="text-right w-full">
+        <Button />
+      </div>
+    </header>
   );
 }
